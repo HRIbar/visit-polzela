4c36aeb9417039354af757e8b13b558f8197b7e8
HRIbar <klemen.hribar@live.com>
2024-12-27 22:55:07 +0100

    corrected theme name, added PWA anotation

--- src/main/java/com/example/starter/base/AppShell.java
+++ src/main/java/com/example/starter/base/AppShell.java
@@ -1,9 +1,14 @@
 package com.example.starter.base;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 
-@Theme("my-theme") // Replace "my-theme" with your actual theme name
+@Theme("starter-theme")
+@PWA(name = "Visit Polzela Progressive Web Application",
+        shortName = "VisitPWA",
+        manifestPath = "manifest.json",
+        iconPath = "icons/icon.png")
 public class AppShell implements AppShellConfigurator {
     // You can add other configurations here if needed
 }
